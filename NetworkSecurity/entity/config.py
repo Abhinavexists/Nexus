a17f96502426365cc3a52651ed397284bcef104c
Abhinavexists <abhinavkumarsingh2023@gmail.com>
2025-10-22 02:24:15 +0530

    added data ingestion components, setup artifact and config, init main.py

--- NetworkSecurity/entity/config.py
+++ NetworkSecurity/entity/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from datetime import datetime
 from NetworkSecurity.constant import training_pipeline
-from exception.exception import CustomException
+from NetworkSecurity.exception.exception import CustomException
 
 # The problem: mutable/default argument trap
 # datetime.now() is evaluated once at function definition time, not each time when create a new object.
@@ -20,37 +20,43 @@ from exception.exception import CustomException
 
 class TrainingPipeineConfig:
     def __init__(self, timestamp:str | None = None):
-        if timestamp is None:
-            timestamp = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+        try:
+            if timestamp is None:
+                timestamp = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
-        self.pipeline_name = training_pipeline.PIPELINE_NAME
-        self.artifact_name = training_pipeline.ARTIFACT_DIR
-        self.artifact_dir: Path = Path(self.artifact_name) / timestamp
-        self.timestamp: str = timestamp
+            self.pipeline_name = training_pipeline.PIPELINE_NAME
+            self.artifact_name = training_pipeline.ARTIFACT_DIR
+            self.artifact_dir: Path = Path(self.artifact_name) / timestamp
+            self.timestamp: str = timestamp
+        except Exception as e:
+            raise CustomException(e)
         
 
 class DataIngestionConfig:
     def __init__(self, training_pipeline_config: TrainingPipeineConfig):
-        self.data_ingestion_dir: Path = Path(training_pipeline_config.artifact_dir) / training_pipeline.DATA_INGESTION_DIR_NAME
+        try:
+            self.data_ingestion_dir: Path = Path(training_pipeline_config.artifact_dir) / training_pipeline.DATA_INGESTION_DIR_NAME
 
-        self.feature_store_file_path: Path = (
-            self.data_ingestion_dir /
-            training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR /
-            training_pipeline.FILE_NAME
-        )
+            self.feature_store_file_path: Path = (
+                self.data_ingestion_dir /
+                training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR /
+                training_pipeline.FILE_NAME
+            )
 
-        self.training_file_path: Path = (
-            self.data_ingestion_dir /
-            training_pipeline.DATA_INGESTION_INGESTED_DIR /
-            training_pipeline.TRAIN_FILE_NAME
-        )
+            self.training_file_path: Path = (
+                self.data_ingestion_dir /
+                training_pipeline.DATA_INGESTION_INGESTED_DIR /
+                training_pipeline.TRAIN_FILE_NAME
+            )
 
-        self.testing_file_path: Path = (
-            self.data_ingestion_dir /
-            training_pipeline.DATA_INGESTION_INGESTED_DIR /
-            training_pipeline.TEST_FILE_NAME
-        )
+            self.testing_file_path: Path = (
+                self.data_ingestion_dir /
+                training_pipeline.DATA_INGESTION_INGESTED_DIR /
+                training_pipeline.TEST_FILE_NAME
+            )
 
-        self.train_test_split_ratio = training_pipeline.DATA_INGESTION_TEST_TRAIN_SPLIT_RATION
-        self.collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
-        self.database_name = training_pipeline.DATA_INGESTION_DATABASE_NAME
+            self.train_test_split_ratio = training_pipeline.DATA_INGESTION_TEST_TRAIN_SPLIT_RATION
+            self.collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
+            self.database_name = training_pipeline.DATA_INGESTION_DATABASE_NAME
+        except Exception as e:
+            raise CustomException(e)
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from mlops-project!")
-
+import sys
+from NetworkSecurity.components.data_ingestion import DataIngestion
+from NetworkSecurity.exception.exception import CustomException
+from NetworkSecurity.logging.logging import logging
+from NetworkSecurity.entity.artifact import DataIngestionArtifact
+from NetworkSecurity.entity.config import DataIngestionConfig, TrainingPipeineConfig
 
 if __name__ == "__main__":
-    main()
+    try:
+        training_pipeline_config = TrainingPipeineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
+        logging.info('Initate the data Ingestion')
+
+        data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
+
+    except Exception as e:
+        raise CustomException(e)
